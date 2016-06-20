1cba02a2d597345f03e75a45e3a850d3bd4f0ee0
journey.ad <ad@imjad.cn>
2016-06-20 19:38:15 +0800

    fix

--- hitokoto/update.php
+++ hitokoto/update.php
@@ -1,4 +1,8 @@
-<?php if ($_GET['aaa'] != 'bbb') header('HTTP/1.1 403 Forbidden'); ?>
+<?php if ($_GET['ex'] != 'update'){
+    header('HTTP/1.1 403 Forbidden');
+    exit;
+}
+?>
 <?php
 
 function curl_get_contents($url) { 
